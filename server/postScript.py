7ecec81f86e81f1be22881db6d09e8b538290601
here <hendrik.reiter@ppi.de>
2022-04-21 15:06:50 +0200

    script angepasst

--- server/postScript.py
+++ server/postScript.py
@@ -1,17 +1,19 @@
 from threading import Thread
 import requests
 
-def do_parallel_task(delay, method, args=None):
-    thread = Thread(target=method(*args),
-                    args=(delay, method, args))
+def do_parallel_task(method):
+    thread = Thread(target=method)
     thread.daemon = True
     thread.start()
 
-def contact_another_server(URI, params_dict, srv_ip):
-    res = requests.post('http://{}{}'.format(srv_ip, URI), data=params_dict)
-    requests.post('http://{}{}'.format(srv_ip, URI), data=params_dict.dict)
+def contact_another_server():
+    requests.post('http://{}{}'.format("10.1.0.1", "/add_entry/propagate"), data={"entry": "Entry"})
 
 
 def do_magic():
     for i in range(10):
-        do_parallel_task(contact_another_server, args=("10.1.0.1", "/add_entry/propagate", "POST", {"entry": "Entry"}))
+        print(str(i + 1) + "th try")
+        do_parallel_task(contact_another_server)
+
+if __name__ == '__main__':
+    do_magic()
